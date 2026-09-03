84714a678602898ac9a6566bde68e3f7c6e5a400
shgnaka <90955191+shgnaka@users.noreply.github.com>
2026-09-03 12:21:06 +0900

    診断ログ保存をアプリ初期化時に有効化

--- app/src/main/java/cn/liyuyu/fuckwxscan/App.kt
+++ app/src/main/java/cn/liyuyu/fuckwxscan/App.kt
@@ -2,6 +2,7 @@ package cn.liyuyu.fuckwxscan
 
 import android.app.Application
 import android.content.Intent
+import cn.liyuyu.fuckwxscan.screenshot.ScreenshotDiagnosticLog
 
 /**
  * Created by frank on 2022/10/17.
@@ -10,5 +11,10 @@ class App : Application() {
 
     companion object {
         var screenCaptureIntentResult: Intent? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        ScreenshotDiagnosticLog.init(this)
     }
 }
